347fdc733ee161424794547a2c360a2fe1c46c79
Chittanat <jittanadunited@mail.com>
2026-01-21 17:03:35 +0700

    fghj./

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,18 @@
 // Write your code below
+const input:number = Number(process.argv[2]);
+if (!input || input < 0 ||input > 100){
+        console.log("Invalid input")
+       
+
+}else if(input <=49) {
+    console.log("Grade is F")
+}else if(input <=59) {
+    console.log("Grade is D")
+}else if(input <=69) {
+    console.log("Grade is C")
+}else if(input <=79) {
+    console.log("Grade is B")
+}else {
+    console.log("Grade is A")
+}
+
--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -1,5 +1,9 @@
 const comment = "TypeScript is a strongly typed programming language"
 
-function shortenComment(comment: string): string {
+
+export function shortenComment(comment: string) {
   // Write your code below
+  const text = comment.split(' ')
+  const process:string[]=  text.filter(f=>f.length >=5 && f.length <=10 );
+  return process
 }
